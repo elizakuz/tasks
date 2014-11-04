52c10b1c3d5cc588207b3afcd4bc53d40e46693a
elizakuz <kuzminaeliz@gmail.com>
2014-11-05 00:46:22 +0400

    stepen

---
+++ stepen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	int n;
+	int r = 1;
+	scanf("%d",&a);
+	scanf("%d",&n);
+	while (n)
+	{
+		if (n & 1)
+		{
+			r = r * a;
+		}
+		a = a * a;
+		n >>= 1;
+	}
+	printf("%d\n", r);
+}
