52545a6a216b5009b6dd55053ee62117effe6210
elizakuz <kuzminaeliz@gmail.com>
2014-11-19 10:29:15 +0400

    float 3

--- 3-way.c
+++ 3-way.c
@@ -19,30 +19,38 @@ int main(){
 	scanf("%f", &fval1);
 	scanf("%f", &fval2);
 	val.fval = fval1 / fval2;
-	printf("%d\n", val.data.a);
-	printf("%d\n", val.data.b);
-	printf("%d\n", val.data.c); 
-	printf("result: ");
-	if (val.data.a == 0 & val.data.b == 0 & val.data.c == 0)
+	printf("Знак: %d\n", val.data.a);
+	printf("Экспонента: %d\n", val.data.b);
+	float st = 1, r = 0, ch;
+	for (int i = 1; i <= 23; i++){
+		st = st / 2;
+		ch = ((val.data.c >> 22) & 1) * st; 
+		r = r + ch;
+		val.data.c = (val.data.c & 0x3FFFFF) << 1;
+	}
+	//printf("%d\n", val.data.c);
+	printf("Мантисса: %f\n", r); 
+	printf("Результат: ");
+	if (val.data.a == 0 & val.data.b == 0 & r == 0)
 	{
 		printf("0\n");
 	}
 	else
 	{
-		if (val.data.b == 255 & val.data.c == 0)
+		if (val.data.b == 255 & r == 0)
 		{
-			printf("беск.\n");
+			if (val.data.a == 0) printf("+бесконечность\n");
+			else printf("-бесконечность\n");
 		}
-		else if (val.data.b == 255 & val.data.c != 0)
+		else if (val.data.b == 255 & r != 0)
 		{
 			printf("Not a Number\n");
 		}
 		else 
 		{
-			val.data.b = val.data.b - 127;
 			if ( val.data.a == 1 ) printf("-");
-			printf("1.%d",val.data.c);
-			printf("* 2^%d\n",val.data.b);
+			printf("%f",r + 1);
+			printf("* 2^(%d - 127)\n", val.data.b);
 		}
 	}
 }
